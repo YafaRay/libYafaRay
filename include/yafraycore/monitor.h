4f220899b34c6c26de54b86b0d5d512885970529
Rodrigo Placencia <darknesstide@gmail.com>
2010-02-20 02:36:47 +0000

    - ProgressBar_t: Added comment to indicate that this is an abstract class

--- include/yafraycore/monitor.h
+++ include/yafraycore/monitor.h
@@ -5,19 +5,19 @@
 #include <yafray_config.h>
 
 __BEGIN_YAFRAY
-
+//! Progress bar abstract class with pure virtual members
 class YAFRAYCORE_EXPORT progressBar_t
 {
 	public:
 		virtual ~progressBar_t(){}
 		//! initialize (or reset) the monitor, give the total number of steps that can occur
-		virtual void init(int totalSteps)=0;
+		virtual void init(int totalSteps) = 0;
 		//! update the montior, increment by given number of steps; init has to be called before the first update.
-		virtual void update(int steps=1)=0;
+		virtual void update(int steps = 1) = 0;
 		//! finish progress bar. It could output some summary, simply disappear from GUI or whatever...
-		virtual void done()=0;
+		virtual void done() = 0;
 		//! method to pass some informative text to the progress bar in case needed
-		virtual void setTag(const char* text)=0;
+		virtual void setTag(const char* text) = 0;
 };
 
 /*! the default console progress bar (implemented in console.cc)
