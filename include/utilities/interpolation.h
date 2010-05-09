da678a52d812f05f5b43036c8f1894d998f2ad13
Rodrigo Placencia <rodrigo@darkness.(none)>
2010-05-09 05:01:13 -0500

    - Utilities: refactored cubic interpolation function as a template function

--- include/utilities/interpolation.h
+++ include/utilities/interpolation.h
@@ -36,12 +36,13 @@ inline static double CosineInterpolate(
 	return ( y1 * (1 - mu2) + y2 * mu2);
 }
 
-inline static double CubicInterpolate(
-		double y0,double y1,
-		double y2,double y3,
+template <class T>
+inline T CubicInterpolate(
+		const T &y0, const T &y1,
+		const T &y2, const T &y3,
 		double mu)
 {
-	double a0,a1,a2,a3,mu2;
+	T a0,a1,a2,a3,mu2;
 
 	mu2 = mu*mu;
 	a0 = y3 - y2 - y0 + y1;
@@ -49,7 +50,7 @@ inline static double CubicInterpolate(
 	a2 = y2 - y0;
 	a3 = y1;
 
-	return(a0*mu*mu2+a1*mu2+a2*mu+a3);
+	return (a0*mu*mu2+a1*mu2+a2*mu+a3);
 }
 
 __END_YAFRAY
