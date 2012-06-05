d13046720cf7c1efbc4f1d3f01496f56d7e5cecf
subcomandante <subcomandante@sturmtruppen.(none)>
2012-06-06 00:09:42 +0200

    Moved FAST_MATH and FAST_TRIG defines to cmake

--- include/utilities/mathOptimizations.h
+++ include/utilities/mathOptimizations.h
@@ -53,9 +53,6 @@
 //# define M_SQRT2	1.41421356237309504880	/* sqrt(2) */
 //# define M_SQRT1_2	0.70710678118654752440	/* 1/sqrt(2) */
 
-#define FAST_MATH
-#define FAST_TRIG
-
 __BEGIN_YAFRAY
 
 #define M_2PI		6.28318530717958647692
