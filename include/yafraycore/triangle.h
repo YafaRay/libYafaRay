56135bc41ff6cfb354a07391c5366be45713a61e
Rodrigo Placencia <rodrigo@morestarch.com>
2013-04-20 20:33:42 -0500

    White space stuff

--- include/yafraycore/triangle.h
+++ include/yafraycore/triangle.h
@@ -29,7 +29,7 @@ class YAFRAYCORE_EXPORT triangle_t
 	friend class scene_t;
 	friend class triangleObject_t;
 	friend class triangleInstance_t;
-	
+
 	public:
 		triangle_t(): pa(-1), pb(-1), pc(-1), na(-1), nb(-1), nc(-1), mesh(NULL) { /* Empty */ }
         triangle_t(int ia, int ib, int ic, triangleObject_t* m): pa(ia), pb(ib), pc(ic), na(-1), nb(-1), nc(-1), mesh(m) { /* Empty */ }
@@ -39,22 +39,32 @@ class YAFRAYCORE_EXPORT triangle_t
 		virtual bool clippingSupport() const{ return true; }
 		// return: false:=doesn't overlap bound; true:=valid clip exists
 		virtual bool clipToBound(double bound[2][3], int axis, bound_t &clipped, void *d_old, void *d_new) const;
-		virtual const material_t* getMaterial() const { return material; }	
+		virtual const material_t* getMaterial() const { return material; }
 		virtual void getSurface(surfacePoint_t &sp, const point3d_t &hit, intersectData_t &data) const;
 		virtual float surfaceArea() const;
 		virtual void sample(float s1, float s2, point3d_t &p, vector3d_t &n) const;
-		
+
 		virtual vector3d_t getNormal() const{ return vector3d_t(normal); }
 		void setVertexIndices(int a, int b, int c){ pa=a, pb=b, pc=c; }
 		void setMaterial(const material_t *m) { material = m; }
 		void setNormals(int a, int b, int c){ na=a, nb=b, nc=c; }
 		virtual void recNormal();
+		size_t getIndex() const { return selfIndex; }
+        bool operator == (triangle_t const& a) const
+        {
+            return selfIndex == a.selfIndex;
+        }
+        friend std::ostream & operator << (std::ostream &out,const triangle_t &t)
+        {
+            out << "[ idx = " << t.selfIndex << " (" << t.pa << "," << t.pb << "," << t.pc << ")]";
+            return out;
+        }
 
 	private:
 		int pa, pb, pc; //!< indices in point array, referenced in mesh.
 		int na, nb, nc; //!< indices in normal array, if mesh is smoothed.
 		const material_t* material;
-		normal_t normal; //!< the geometric normal
+		vector3d_t normal; //!< the geometric normal
         const triangleObject_t* mesh;
 		size_t selfIndex;
 };
@@ -63,7 +73,7 @@ class YAFRAYCORE_EXPORT triangleInstance_t: public triangle_t
 {
 	friend class scene_t;
 	friend class triangleObjectInstance_t;
-	
+
 	public:
 		triangleInstance_t(): mBase(NULL), mesh(NULL) { }
         triangleInstance_t(triangle_t* base, triangleObjectInstance_t* m): mBase(base), mesh(m) { }
@@ -73,11 +83,11 @@ class YAFRAYCORE_EXPORT triangleInstance_t: public triangle_t
 		virtual bool clippingSupport() const { return true; }
 		// return: false:=doesn't overlap bound; true:=valid clip exists
 		virtual bool clipToBound(double bound[2][3], int axis, bound_t &clipped, void *d_old, void *d_new) const;
-		virtual const material_t* getMaterial() const { return mBase->getMaterial(); }	
+		virtual const material_t* getMaterial() const { return mBase->getMaterial(); }
 		virtual void getSurface(surfacePoint_t &sp, const point3d_t &hit, intersectData_t &data) const;
 		virtual float surfaceArea() const;
 		virtual void sample(float s1, float s2, point3d_t &p, vector3d_t &n) const;
-		
+
 		virtual vector3d_t getNormal() const;
 		virtual void recNormal() { /* Empty */ };
 
@@ -103,9 +113,9 @@ class YAFRAYCORE_EXPORT vTriangle_t: public primitive_t
 		virtual bool clippingSupport() const { return true; }
 		// return: false:=doesn't overlap bound; true:=valid clip exists
 		virtual bool clipToBound(double bound[2][3], int axis, bound_t &clipped, void *d_old, void *d_new) const;
-		virtual const material_t* getMaterial() const { return material; }	
+		virtual const material_t* getMaterial() const { return material; }
 		virtual void getSurface(surfacePoint_t &sp, const point3d_t &hit, intersectData_t &data) const;
-		
+
 		// following are methods which are not part of primitive interface:
 		void setMaterial(const material_t *m) { material = m; }
 		void setNormals(int a, int b, int c){ na=a, nb=b, nc=c; }
@@ -135,9 +145,9 @@ class YAFRAYCORE_EXPORT bsTriangle_t: public primitive_t
 		//virtual bool intersectsBound(exBound_t &eb) const;
 		// return: false:=doesn't overlap bound; true:=valid clip exists
 		//virtual bool clipToBound(double bound[2][3], int axis, bound_t &clipped, void *d_old, void *d_new) const;
-		virtual const material_t* getMaterial() const { return material; }	
+		virtual const material_t* getMaterial() const { return material; }
 		virtual void getSurface(surfacePoint_t &sp, const point3d_t &hit, intersectData_t &data) const;
-		
+
 		// following are methods which are not part of primitive interface:
 		void setMaterial(const material_t *m) { material = m; }
 		void setNormals(int a, int b, int c){ na=a, nb=b, nc=c; }
