76cd3441ca118adfb05a8945cfd46a2e3a77cceb
DavidBluecame <david.bluecame@gmail.com>
2016-02-06 15:43:55 +0100

    Version change to v2.1.1

--- include/yafray_constants.h
+++ include/yafray_constants.h
@@ -5,7 +5,7 @@
 #define __END_YAFRAY }
 
 #define PACKAGE "YafaRay"
-#define VERSION "Experimental 2.1.0"
+#define VERSION "Experimental 2.1.1"
 
 #if (__GNUC__ > 3)
         #define GCC_HASCLASSVISIBILITY
