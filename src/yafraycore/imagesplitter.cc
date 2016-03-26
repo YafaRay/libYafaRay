8a165d37eeef51d13b61334e80a91f594f85c1a8
DavidBluecame <david.bluecame@gmail.com>
2016-03-26 18:42:51 +0100

    Automatic reduction of the tile size in the last tiles As requested in http://yafaray.org/node/709 In this first implementation, the last 16 tiles will be automatically subdivided to smaller 4x4 tiles Perhaps in the near future I will implement parameters to control this and/or a more refined way of doing it.

--- src/yafraycore/imagesplitter.cc
+++ src/yafraycore/imagesplitter.cc
@@ -14,6 +14,9 @@ imageSpliter_t::imageSpliter_t(int w, int h, int x0,int y0, int bsize, tilesOrde
 	int nx, ny;
 	nx = (w+blocksize-1)/blocksize;
 	ny = (h+blocksize-1)/blocksize;
+
+	std::vector<region_t> regions_raw;
+
 	for(int j=0; j<ny; ++j)
 	{
 		for(int i=0; i<nx; ++i)
@@ -23,14 +26,42 @@ imageSpliter_t::imageSpliter_t(int w, int h, int x0,int y0, int bsize, tilesOrde
 			r.y = y0 + j*blocksize;
 			r.w = std::min(blocksize, x0+w-r.x);
 			r.h = std::min(blocksize, y0+h-r.y);
-			regions.push_back(r);
+			regions_raw.push_back(r);
 		}
 	}
 	switch(tilesorder)
 	{
-		case RANDOM:	std::random_shuffle( regions.begin(), regions.end() );
+		case RANDOM:	std::random_shuffle( regions_raw.begin(), regions_raw.end() );
 		case LINEAR:
 		default:	break;
+	}
+	
+	for(size_t rn=0; rn<regions_raw.size(); ++rn)
+	{
+		if(blocksize <= 4 || rn<regions_raw.size()-16)	//If blocksize is more than 4, resubdivide the last 16 tiles so their block size is 4 (better CPU/thread usage in the last tiles to avoid having one big tile at the end with only 1 CPU thread)
+		{
+			regions.push_back(regions_raw[rn]);
+		}
+		else
+		{
+			int blocksize2 = 4;
+			int nx2, ny2;
+			nx2 = (regions_raw[rn].w+blocksize2-1)/blocksize2;
+			ny2 = (regions_raw[rn].h+blocksize2-1)/blocksize2;
+
+			for(int j=0; j<ny2; ++j)
+			{
+				for(int i=0; i<nx2; ++i)
+				{
+					region_t r;
+					r.x = regions_raw[rn].x + i*blocksize2;
+					r.y = regions_raw[rn].y + j*blocksize2;
+					r.w = std::min(blocksize2, regions_raw[rn].x+regions_raw[rn].w-r.x);
+					r.h = std::min(blocksize2, regions_raw[rn].y+regions_raw[rn].h-r.y);
+					regions.push_back(r);
+				}
+			}
+		}
 	}
 }
 
