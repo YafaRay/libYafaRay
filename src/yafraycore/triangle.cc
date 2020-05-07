08b7ecfcd4ff81feb534494b037de78b7ab6c3d7
DavidBluecame <david.bluecame@gmail.com>
2020-05-07 06:39:09 +0200

    Triangle: added "mesh->normals_exported" condition to enable user defined normals As requested by the developer of Sweet Home 3D: https://github.com/puybaret

--- src/yafraycore/triangle.cc
+++ src/yafraycore/triangle.cc
@@ -16,7 +16,7 @@ inline void triangle_t::getSurface(surfacePoint_t &sp, const point3d_t &hit, int
 
 	float u = data.b0, v = data.b1, w = data.b2;
 
-	if(mesh->is_smooth)
+	if(mesh->is_smooth || mesh->normals_exported)
 	{
 		vector3d_t va = (na >= 0) ? mesh->getVertexNormal(na) : sp.Ng;
 		vector3d_t vb = (nb >= 0) ? mesh->getVertexNormal(nb) : sp.Ng;
@@ -644,7 +644,7 @@ void bsTriangle_t::getSurface(surfacePoint_t &sp, const point3d_t &hit, intersec
 	float u = data.b0, v = data.b1, w = data.b2;
 
 	//todo: calculate smoothed normal...
-	/* if(mesh->is_smooth)
+	/* if(mesh->is_smooth || mesh->normals_exported)
 	{
 		vector3d_t va(na>0? mesh->normals[na] : normal), vb(nb>0? mesh->normals[nb] : normal), vc(nc>0? mesh->normals[nc] : normal);
 		sp.N = u*va + v*vb + w*vc;
